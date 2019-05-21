68103386efd4c165f47dc37e7882be99214c63b4
merlambangbuana <blacklistbuana@gmail.com>
2019-05-21 20:49:23 +0700

    first commit

---
+++ about us.php
@@ -0,0 +1,80 @@
+<html>
+<head>
+<style>
+<?php include 'css/about us.css'; ?>
+</style>
+</head>
+<body bgcolor="#f1f1f1">
+
+	<div class="kolom12">
+		<div class="header">
+		<a href="index.php"><img class="logo" src="images/logo.png" alt="logo" width="10%" height="8%"></a>
+			<div class="dropdown margindropdown">
+				<button class="dropbtn"><a href="index.php">Admin</a></button>
+					
+					
+			</div>
+			<div class="dropdown margindropdown">
+				<button class="dropbtn"><a href="about us.php">About Us</a></button>
+					
+					
+			</div>
+			<div class="dropdown margindropdown">
+				<button class="dropbtn"><a href="contactus.php">Contact Us</a></button>
+					
+					
+						
+					
+						
+					</div>
+					
+			</div>
+		</div>
+	</div>
+<div class="main">
+
+<h1> About Us</h1>
+<hr>
+
+
+<div class="row">
+  <div class="column">
+    <div class="content">
+      <img src="images/3.jpg" alt="Desa Waerebo" style="width:100%">
+      <h3>Alfin Rizqiansi Ansory</h3>
+      <p>Alfin adalah seorang Mahasiswa Universitas Brawijaya jurusan Manejemen Informatika Semester 2. Pria kelahiran 7 Juli 1999 ini berasal dari Bojonegoro.</p>  <a href="https://www.instagram.com/erlambangbuana/"><img class="sosial-media" src ="ig.png" width="15%"></a>
+	</div>
+  </div>
+  <div class="column">
+    <div class="content">
+    <img src="images/1.jpg" alt="raja ampat" style="width:100%">
+      <h3>Muhamad Erlambang Buana</h3>
+      <p>Er adalah seorang Mahasiswa Universitas Brawijaya jurusan Manejemen Informatika Semester 2. Pria kelahiran 23 April 1999 ini berasal dari Pasuruan.</p>
+    <a href="https://www.instagram.com/erlambangbuana/"><img class="sosial-media" src ="ig.png" width="15%"></a>
+	</div>
+  </div>
+  <div class="column">
+    <div class="content">
+    <img src="images/2.jpg" alt="Grand Canyon" style="width:100%">
+     <h3>Ema Yunian Apriliyanti</h3>
+      <p>Ema adalah seorang Mahasiswi Universitas Brawijaya jurusan Manajemen Informatika semester 2. Wanita kelahiran 20 April 1999 ini berasal dari Ngawi</p>
+    <a href="https://www.instagram.com/erlambangbuana/"><img class="sosial-media" src ="ig.png" width="15%"></a>
+	</div>
+  </div>
+
+</div>
+
+</div>
+
+		<div class="footer">
+		<p>Alfin Rizqiansi Ansory</p>
+		<a href="https://www.facebook.com/nub.bun"><img class="sosial-media" src ="images/fb.png" width="3%"></a>
+		<a href="https://www.instagram.com/erlambangbuana/"><img class="sosial-media" src ="images/insta.png" width="3%"></a>
+
+		<p>Ema Yunian Apriliyanti</p>
+		<p>Muhamad Erlambang Buana</p>
+					
+		</div>
+
+</body>
+</html>
